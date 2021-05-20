cf62b3535fdb56191c0dbe74c4a7afd9af5ce289
Batya02 <magomedov.abdul20012@gmail.com>
2021-05-20 13:22:45 +0300

    changes

--- payment/payment.py
+++ payment/payment.py
@@ -10,7 +10,7 @@ from config import config
 cfg = config.Config()
 
 DateTime = dt.now(tz=tzlocal()) + timedelta(minutes=15)
-DT = DateTime.strftime("%Y-%m-%dT%H:%M:%S+00:00")
+DT = DateTime.strftime("%Y-%m-%dT%H:%M:%S+03:00")
 
 class Payment:
     headers:dict
